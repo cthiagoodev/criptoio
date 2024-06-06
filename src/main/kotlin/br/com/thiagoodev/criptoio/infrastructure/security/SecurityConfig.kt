1120cf696884ef307ed0fe53e760bffd782b27c9
thiagosousadev <thiagosousadev@gmail.com>
2024-06-06 17:30:26 -0300

    alteracoes

--- src/main/kotlin/br/com/thiagoodev/criptoio/infrastructure/security/SecurityConfig.kt
+++ src/main/kotlin/br/com/thiagoodev/criptoio/infrastructure/security/SecurityConfig.kt
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -19,6 +20,7 @@ class SecurityConfig(private val userDetailsService: UserDetailsService) {
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authenticationProvider(authenticationProvider())
             .authorizeHttpRequests {
                 it.anyRequest().permitAll()

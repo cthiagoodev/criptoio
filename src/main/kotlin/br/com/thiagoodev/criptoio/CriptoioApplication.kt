7c9e708858ff72794f23bc95c4e6ccb1385a8bcd
thiagosousadev <thiagosousadev@gmail.com>
2024-06-17 17:22:57 -0300

    alteracoes

--- src/main/kotlin/br/com/thiagoodev/criptoio/CriptoioApplication.kt
+++ src/main/kotlin/br/com/thiagoodev/criptoio/CriptoioApplication.kt
@@ -2,10 +2,12 @@ package br.com.thiagoodev.criptoio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories
 class CriptoioApplication
 
 fun main(args: Array<String>) {
